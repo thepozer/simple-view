9ea048d30cea66ea9e6f53366c9b4951baeb98dd
Didier 'Pozer' Prolhac <dev@thepozer.net>
2021-01-27 13:43:53 +0100

    Add type on params and returns and force PHP 7.2

--- src/SimpleView.php
+++ src/SimpleView.php
@@ -26,164 +26,160 @@ namespace Thepozer\View ;
 use Psr\Http\Message\ResponseInterface;
 
 class SimpleView implements \ArrayAccess, \Countable, \IteratorAggregate {
-	private $arValues = array();
-	
-	private $sViewDir      = null;
-	private $sViewSuffix   = null;
-	private $sViewTemplate = null;
-	
-	private $sViewFilename = null;
+    private $arValues = array();
 
-	private $sCurrentView = null;
+    private $sViewDir      = null;
+    private $sViewSuffix   = null;
+    private $sViewTemplate = null;
 
-	public function __construct ($sViewDir = 'views/', $sViewSuffix = '.phtml', $sViewTemplate = '_template.phtml') {
-		$this->sViewDir      = $sViewDir;
-		$this->sViewSuffix   = $sViewSuffix;
-		$this->sViewTemplate = $sViewTemplate;
-	}
-	
-	/**
-	 * Output rendered template
-	 *
-	 * @param ResponseInterface $response
-	 * @param  string $template Template pathname relative to templates directory
-	 * @param  array $data Associative array of template variables
-	 * @return ResponseInterface
-	 */
-	public function render(ResponseInterface $oResponse, $sView, $arData = [])
-	{
-		 $oResponse->getBody()->write($this->fetch($sView, $arData));
-		 return $oResponse;
-	}
-	
-	private function isCurrentView($mViewsNames) {
-		$arViewsNames = (is_array($mViewsNames)) ? $mViewsNames : [$mViewsNames];
+    private $sViewFilename = null;
 
-		return in_array($this->sCurrentView, $arViewsNames);
-	}
+    private $sCurrentView = null;
 
-	private function fetch($sView, $arData) {
-		$arConfig = array('useTemplate' => true);
-		if (array_key_exists('_conf', $arData)) {
-			$arConfig = array_merge($arConfig, $arData['_conf']);
-			unset($arData['_conf']);
-		}
-		
-		$this->sCurrentView = $sView;
-		$this->arValues = array_merge($this->arValues, $arData);
-		
-		if ($arConfig['useTemplate']) {
-			$sTemplateView = $this->sViewDir . $this->sViewTemplate;
-			$this->sViewFilename = $this->sViewDir . $sView . $this->sViewSuffix;
-		} else {
-			$this->sViewFilename = $sTemplateView = $this->sViewDir . $sView . $this->sViewSuffix;
-			
-		}
-		if (file_exists($sTemplateView) && file_exists($this->sViewFilename)) {
-			ob_start();
-			require $sTemplateView;
-			return ob_get_clean();
-		}
-		
-		return '<h1>ERROR : View not found</h1>';
-	}
-	
-	private function includeView() {
-		require $this->sViewFilename;
-	}
-	
-	/********************************************************************************
-	 * Magic functions
-	 *******************************************************************************/
-	
-    public function __set($sName, $mValue) {
-		$this->offsetSet($sName, $mValue);
+    public function __construct (string $sViewDir = 'views/', string $sViewSuffix = '.phtml', string $sViewTemplate = '_template.phtml') {
+        $this->sViewDir      = $sViewDir;
+        $this->sViewSuffix   = $sViewSuffix;
+        $this->sViewTemplate = $sViewTemplate;
     }
-    
-	public function __get($sName) {
-		return $this->offsetGet($sName);
-	}
-	
-    public function __isset($sName) {
+
+    /**
+     * Output rendered template
+     *
+     * @param ResponseInterface $response
+     * @param  string $template Template pathname relative to templates directory
+     * @param  array $data Associative array of template variables
+     * @return ResponseInterface
+     */
+    public function render(ResponseInterface $oResponse, string $sView, array $arData = []): ResponseInterface {
+        $oResponse->getBody()->write($this->fetch($sView, $arData));
+        return $oResponse;
+    }
+
+    private function isCurrentView($mViewsNames): boolean {
+        $arViewsNames = (is_array($mViewsNames)) ? $mViewsNames : [$mViewsNames];
+
+        return in_array($this->sCurrentView, $arViewsNames);
+    }
+
+    private function fetch(string $sView, array $arData) : string {
+        $arConfig = array('useTemplate' => true);
+        if (array_key_exists('_conf', $arData)) {
+            $arConfig = array_merge($arConfig, $arData['_conf']);
+            unset($arData['_conf']);
+        }
+
+        $this->sCurrentView = $sView;
+        $this->arValues = array_merge($this->arValues, $arData);
+
+        if ($arConfig['useTemplate']) {
+            $sTemplateView = $this->sViewDir . $this->sViewTemplate;
+            $this->sViewFilename = $this->sViewDir . $sView . $this->sViewSuffix;
+        } else {
+            $this->sViewFilename = $sTemplateView = $this->sViewDir . $sView . $this->sViewSuffix;
+        }
+
+        if (file_exists($sTemplateView) && file_exists($this->sViewFilename)) {
+            ob_start();
+            require $sTemplateView;
+            return ob_get_clean();
+        }
+
+        return '<h1>ERROR : View not found</h1>';
+    }
+
+    private function includeView() {
+        require $this->sViewFilename;
+    }
+
+    /********************************************************************************
+     * Magic functions
+     *******************************************************************************/
+
+    public function __set(string $sName, $mValue) {
+        $this->offsetSet($sName, $mValue);
+    }
+
+    public function __get(string $sName) {
+        return $this->offsetGet($sName);
+    }
+
+    public function __isset(string $sName) {
         return $this->offsetExists($sName);
     }
 
-    public function __unset($sName) {
+    public function __unset(string $sName) {
         $this->offsetUnset($sName);
     }
-    
-	/********************************************************************************
-	 * ArrayAccess interface
-	 *******************************************************************************/
-	
-	/**
-	 * Does this collection have a given key?
-	 *
-	 * @param string $sName The data key
-	 *
-	 * @return bool
-	 */
-	public function offsetExists($sName)
-	{
-		return array_key_exists($sName, $this->arValues);
-	}
-	/**
-	 * Get collection item for key
-	 *
-	 * @param string $sName The data key
-	 *
-	 * @return mixed The key's value, or the default value
-	 */
-	public function offsetGet($sName) {
-		if (array_key_exists($sName, $this->arValues)) {
-			return $this->arValues[$sName];
-        } else {
-			throw new \Exception("Undefined property : {$sName}");
-		}
-	}
-	/**
-	 * Set collection item
-	 *
-	 * @param string $sName  The data key
-	 * @param mixed  $mValue The data value
-	 */
-	public function offsetSet($sName, $mValue) {
-		$this->arValues[$sName] = $mValue;
-	}
-	/**
-	 * Remove item from collection
-	 *
-	 * @param string $sName The data key
-	 */
-	public function offsetUnset($sName) {
-		if (array_key_exists($sName, $this->arValues)) {
-			unset($this->arValues[$sName]);
-		}
-	}
-	/********************************************************************************
-	 * Countable interface
-	 *******************************************************************************/
-	/**
-	 * Get number of items in collection
-	 *
-	 * @return int
-	 */
-	public function count()
-	{
-		return count($this->arValues);
-	}
-	
-	/********************************************************************************
-	 * IteratorAggregate interface
-	 *******************************************************************************/
-	/**
-	 * Get collection iterator
-	 *
-	 * @return \ArrayIterator
-	 */
-	public function getIterator()
-	{
-		return new \ArrayIterator($this->arValues);
-	}
 
+    /********************************************************************************
+     * ArrayAccess interface
+     *******************************************************************************/
+
+    /**
+     * Does this collection have a given key?
+     *
+     * @param string $sName The data key
+     *
+     * @return bool
+     */
+    public function offsetExists(string $sName) {
+        return array_key_exists($sName, $this->arValues);
+    }
+    /**
+     * Get collection item for key
+     *
+     * @param string $sName The data key
+     *
+     * @return mixed The key's value, or the default value
+     */
+    public function offsetGet(string $sName) {
+        if (array_key_exists($sName, $this->arValues)) {
+            return $this->arValues[$sName];
+        } else {
+            throw new \Exception("Undefined property : {$sName}");
+        }
+    }
+    /**
+     * Set collection item
+     *
+     * @param string $sName  The data key
+     * @param mixed  $mValue The data value
+     */
+    public function offsetSet(string $sName, $mValue) {
+        $this->arValues[$sName] = $mValue;
+    }
+    /**
+     * Remove item from collection
+     *
+     * @param string $sName The data key
+     */
+    public function offsetUnset(string $sName) {
+        if (array_key_exists($sName, $this->arValues)) {
+            unset($this->arValues[$sName]);
+        }
+    }
+    /********************************************************************************
+     * Countable interface
+     *******************************************************************************/
+    /**
+     * Get number of items in collection
+     *
+     * @return int
+     */
+    public function count(): int {
+        return count($this->arValues);
+    }
+
+    /********************************************************************************
+     * IteratorAggregate interface
+     *******************************************************************************/
+    /**
+     * Get collection iterator
+     *
+     * @return \ArrayIterator
+     */
+    public function getIterator(): \ArrayIterator {
+        return new \ArrayIterator($this->arValues);
+    }
+ 
 }
